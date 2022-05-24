c9db196898894a054ea0c1afe6f85e7f0134f34e
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-24 16:45:50 -0700

    api call returns data for character searched

--- public/landing.js
+++ public/landing.js
@@ -1,13 +1,15 @@
-const form = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('#search-btn');
+const hero = document.querySelector('#char-search');
+
 
 
 
 function searchCharacter(event) {
-    axios.get(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b6b72ca54e82d39459344ac31c187566&hash=aea080d9b15055a44c83d8469af5da70`)
+    axios.get(`http://gateway.marvel.com/v1/public/characters?name=${hero.value}&ts=1&apikey=b6b72ca54e82d39459344ac31c187566&hash=aea080d9b15055a44c83d8469af5da70`)
     .then((res) => {
         console.log(res.data)
     })
 } 
 
 
-form.addEventListener('click', searchCharacter)
+searchBtn.addEventListener('click', searchCharacter)
