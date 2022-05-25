be71c7e9fe76ce9032ded4801c85feb587c07a72
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-25 07:24:42 -0700

    fixed backend hash and input css

--- public/landing.js
+++ public/landing.js
@@ -7,7 +7,17 @@ const hero = document.querySelector('#char-search');
 function searchCharacter(event) {
     axios.get(`http://gateway.marvel.com/v1/public/characters?name=${hero.value}&ts=1&apikey=b6b72ca54e82d39459344ac31c187566&hash=aea080d9b15055a44c83d8469af5da70`)
     .then((res) => {
-        console.log(res.data)
+        let character = res.data
+        // let characterArr = [];
+        // character.forEach(element => {
+        //     characterArr.push({
+        //         name: character.results.name,
+        //         decription: character.results.description,
+        //         thumbnail: character.results.thumbnail
+        //     })
+        // });
+        console.log(character)
+
     })
 } 
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const md5 = require('md5');
 require('dotenv').config();
@@ -7,7 +8,9 @@ const app = express();
 
 const privateKey = (process.env.PRIVATE_KEY);
 const publicKey = (process.env.PUBLIC_KEY);
-
+const ts = 1;
+const hash = md5(ts, privateKey, publicKey)
+console.log(hash)
 
 
 
@@ -21,9 +24,7 @@ app.get('/styles', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/styles.css'))
 })
 
-app.get('/gateway.marvel.com/v1/public/characters?ts=1&apikey=b6b72ca54e82d39459344ac31c187566&hash=aea080d9b15055a44c83d8469af5da70', (req, res) => {
-    console.log(req.query);
-})
+
 
 
 
