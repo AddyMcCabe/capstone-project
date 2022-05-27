38a1eceb8b4f9912aa7c842f1f67a9178bdf8e04
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-26 21:59:17 -0700

    err alert for char not found

--- public/landing.js
+++ public/landing.js
@@ -9,11 +9,11 @@ const hero = document.querySelector('#char-search');
 function searchCharacter(event) {
     axios.get(`https://gateway.marvel.com/v1/public/characters?name=${hero.value}&ts=1&apikey=b6b72ca54e82d39459344ac31c187566&hash=aea080d9b15055a44c83d8469af5da70`)
     .then((res) => {
+        if(hero.value === res.data) {
         let characterName = res.data.data.results[0].name;
         let characterDescription = res.data.data.results[0].description;
         let characterImage = res.data.data.results[0].thumbnail.path;
         
-        if(characterName) {
          window.localStorage.setItem('name', characterName);
          window.localStorage.setItem('dscrpt', characterDescription);
          window.localStorage.setItem('img', characterImage);
